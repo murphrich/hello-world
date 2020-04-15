de6e8eb6a002726b2f6283e353ec67b1afcbfcbf
Richard <murph.rich@gmail.com>
2020-04-15 16:10:57 +0200

    Changed Carriage return

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * This is a test doc comment
  * @category Test
- * @package New
- * @author Richard
- * @license None
- * @link N/A
+ * @package  New
+ * @author   Richard <richardm@moredigital.co.za>
+ * @license  None
+ * @link     N/A
  */
 
 echo 'Hello Jodi and Carina';
