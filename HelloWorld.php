b273d58897aa3f7b2fe254b75cca2f2cf7c45c7b
Richard <murph.rich@gmail.com>
2020-04-15 16:02:22 +0200

    Changed Carriage return

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,1 +1,8 @@
-<?php/** * Test script that spans multiple * lines should look like this */echo 'Hello Jodi and Carina';phpinfo();
+<?php
+/**
+ * Test script that spans multiple
+ * lines should look like this
+ */
+
+echo 'Hello Jodi and Carina';
+phpinfo();
